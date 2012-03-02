c000ed0ffc90e12f273dc6bed5e0b256685b6565
Mauricio Cirelli <cirelli.mauricio@gmail.com>
2012-03-02 09:30:50 -0300

    gitignore

---
+++ ERP-Palmeiras-RH/ERP-Palmeiras-RH/Controllers/FuncionariosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace ERP_Palmeiras_RH.Controllers
+{
+    /// <summary>
+    /// Controller para Actions de Cadastro de Funcionarios.
+    /// </summary>
+    public class FuncionariosController : Controller
+    {
+        /// <summary>
+        /// Exibe a lista dos funcionários.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            
+        }
+
+        public ActionResult Search()
+        {
+
+        }
+    }
+}
