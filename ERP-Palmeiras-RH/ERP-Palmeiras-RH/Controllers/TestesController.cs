53e1fe238aed318aa9c59c9be49be755ec2dcd58
Mauricio Cirelli <cirelli.mauricio@gmail.com>
2012-03-17 12:52:25 -0300

    ERP-93 - Falta inserir cargos

--- ERP-Palmeiras-RH/ERP-Palmeiras-RH/Controllers/TestesController.cs
+++ ERP-Palmeiras-RH/ERP-Palmeiras-RH/Controllers/TestesController.cs
@@ -32,20 +32,20 @@ namespace ERP_Palmeiras_RH.Controllers
             Beneficio b1 = new Beneficio();
             b1.Nome = "Vale-Refeição";
             b1.Valor = 300;
-            facade.InserirBeneficio(b1);
+            beneficiosFacade.InserirBeneficio(b1);
 
             Beneficio b2 = new Beneficio();
             b2.Nome = "Vale-Transporte";
             b2.Valor = 300;
-            facade.InserirBeneficio(b2);
+            beneficiosFacade.InserirBeneficio(b2);
 
             Especialidade e1 = new Especialidade();
             e1.Nome = "Urologia";
-            facade.InserirEspecialidade(e1);
+            especFacade.InserirEspecialidade(e1);
 
             Especialidade e2 = new Especialidade();
             e2.Nome = "Pediatria";
-            facade.InserirEspecialidade(e2);
+            especFacade.InserirEspecialidade(e2);
 
             return RedirectToAction("CadastrarMedico");
         }
